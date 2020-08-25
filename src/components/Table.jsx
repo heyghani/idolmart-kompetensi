5dca1f5c478bec46641cc3cef32c739ea976d83a
heyghani <heyghani@gmail.com>
2020-08-25 11:44:49 +0700

    update

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,46 +1,95 @@
 import React from "react";
 import { useTable } from "react-table";
+import {
+	TableContainer,
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+	Paper,
+} from "@material-ui/core";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 
-export default function Table({ columns, data }) {
-  // Use the useTable Hook to send the columns and data to build the table
-  const {
-    getTableProps, // table props from react-table
-    getTableBodyProps, // table body props from react-table
-    headerGroups, // headerGroups, if your table has groupings
-    rows, // rows for the table based on the data passed
-    prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
-  } = useTable({
-    columns,
-    data
-  });
+const StyledTableCell = withStyles((theme) => ({
+	head: {
+		backgroundColor: theme.palette.common.black,
+		color: theme.palette.common.white,
+	},
+	body: {
+		fontSize: 14,
+	},
+}))(TableCell);
 
-  /* 
+const StyledTableRow = withStyles((theme) => ({
+	root: {
+		"&:nth-of-type(odd)": {
+			backgroundColor: theme.palette.action.hover,
+		},
+	},
+}))(TableRow);
+
+const useStyles = makeStyles({
+	table: {
+		minWidth: 700,
+	},
+});
+
+export default function ReactTable({ columns, data }) {
+	// Use the useTable Hook to send the columns and data to build the table
+
+	const classes = useStyles();
+
+	const {
+		getTableProps, // table props from react-table
+		getTableBodyProps, // table body props from react-table
+		headerGroups, // headerGroups, if your table has groupings
+		rows, // rows for the table based on the data passed
+		prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
+	} = useTable({
+		columns,
+		data,
+	});
+
+	/* 
     Render the UI for your table
     - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
   */
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+	return (
+		<TableContainer component={Paper}>
+			<Table
+				className={classes.table}
+				aria-label="customized table"
+				{...getTableProps()}
+			>
+				<TableHead>
+					{headerGroups.map((headerGroup) => (
+						<TableRow {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map((column) => (
+								<StyledTableCell align="center" {...column.getHeaderProps()}>
+									{column.render("Header")}
+								</StyledTableCell>
+							))}
+						</TableRow>
+					))}
+				</TableHead>
+				<TableBody {...getTableBodyProps()}>
+					{rows.map((row, i) => {
+						prepareRow(row);
+						return (
+							<StyledTableRow {...row.getRowProps()}>
+								{row.cells.map((cell) => {
+									return (
+										<StyledTableCell align="center" {...cell.getCellProps()}>
+											{cell.render("Cell")}
+										</StyledTableCell>
+									);
+								})}
+							</StyledTableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 }
